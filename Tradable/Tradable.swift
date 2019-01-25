dac7cf2dddfb192503eef7ae6fe10f8cc02a7533
1amageek <tmy0x3@icloud.com>
2019-01-25 14:09:23 +0900

    fix

--- Tradable/Tradable.swift
+++ Tradable/Tradable.swift
@@ -296,12 +296,14 @@ public enum OrderPaymentStatus: String {
 
 public protocol OrderItemProtocol: Document {
     associatedtype SKU: SKUProtocol
+    associatedtype Product: ProductProtocol
     associatedtype Order: OrderProtocol
     associatedtype Person: UserProtocol
     var order: Relation<Order> { get set }
     var purchasedBy: Relation<Person> { get set }
     var selledBy: Relation<Person> { get set }
-    var type: OrderItemType { get set }    // OrderItemType
+    var type: OrderItemType { get set }
+    var prodcut: Relation<Product> { get set }
     var sku: Relation<SKU> { get set }
     var quantity: Int { get set }
     var currency: Currency { get set }
